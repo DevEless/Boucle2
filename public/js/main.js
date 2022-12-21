f52981633bb09f0f55c4ec1bebefc938e5c79c0f
Quent_DLM <qdelameilleure@gmail.com>
2022-12-21 14:49:35 +0100

    stipé

--- public/js/main.js
+++ public/js/main.js
@@ -13,4 +13,35 @@ do {
     Mot = prompt("Saisissez un élément du tableau :");
 } while (!stipé2.includes(Mot) || !Mot.includes("a"));
 
-console.log(`L'élément saisi est : ${Mot}`);
+console.log(`je sapel groot : ${Mot}`);
+
+
+const nombre = [];
+
+
+for (let i = 0; i < 100; i++) {
+    nombre.push(Math.floor(Math.random() * 100));
+}
+
+for (let i = 0; i < nombre.length; i++) {
+    if (nombre[i] < 50) {
+        console.log(`c'est la fin des harico ${i}`);
+        break;
+    }
+    console.log(numbers[i]);
+}
+
+const nombre2 = [];
+
+
+for (let i = 0; i < 100; i++) {
+    nombre2.push(Math.floor(Math.random() * 100));
+}
+
+for (let i = 0; i < nombre2.length; i++) {
+    if (i == 100) {
+        console.log(`une biere offerte au héro qui a arreté la boucle ${i} merci au champion numero: ${nombre2[i]}`);
+        break;
+    }
+    console.log(numbers[i]);
+}
